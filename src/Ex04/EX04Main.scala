7a43e7f60ded61ffa6a55c2c1056e724bb685f47
Maoz M Shechtman <maozshechtman@gmail.com>
2020-05-27 10:39:36 +0300

    Tokenizer complated GUI was added to the program TODO: create the parsing class and finsh EX04

--- src/Ex04/EX04Main.scala
+++ src/Ex04/EX04Main.scala
@@ -1,10 +1,21 @@
 package Ex04
+
 import java.io.File
+
+import javax.swing.{JFileChooser, JFrame}
 
 object EX04Main {
   def main(args: Array[String]): Unit = {
-    val name = "C:\\Users\\user\\Desktop\\שנה ג\\סמסטר ב תשפ\\עקרונות שפות תוכנה\\Labs\\nand2tetris\\projects\\10\\Square"
-    readJackFiles(new File(name))
+    val frame = new JFrame(" Choose directory")
+    val fileChooser = new JFileChooser()
+    fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY)
+    val res = fileChooser.showOpenDialog(null)
+    if (JFileChooser.APPROVE_OPTION == res) {
+      val f = fileChooser.getSelectedFile()
+      //System.err.println(f.getPath())
+    }
+    val directory = fileChooser.getSelectedFile.getPath
+    readJackFiles(new File(directory))
   }
 
   def readJackFiles(directory: File): Unit = {
--- src/Ex04/Tokenizing.scala
+++ src/Ex04/Tokenizing.scala
@@ -3,31 +3,31 @@ package Ex04
 import java.io.{File, FileOutputStream, PrintWriter}
 
 import scala.io.Source
-
+import scala.util.control.Breaks._
 case class Tokenizing(jackFile: File = null) {
   //Atribute
   private final val keyWords = Array("class",
 
-    "constructor ",
-    "function ",
-    "method ",
-    "field ",
-    "static ",
-    "var ",
-    "int ",
-    "char ",
-    "boolean ",
-    "void ",
-    "true ",
-    "false ",
-    "null ",
-    "this ",
-    "let ",
-    "do ",
-    "if ",
-    "else ",
-    "while ",
-    "return ")
+    "constructor",
+    "function",
+    "method",
+    "field",
+    "static",
+    "var",
+    "int",
+    "char",
+    "boolean",
+    "void",
+    "true",
+    "false",
+    "null",
+    "this",
+    "let",
+    "do",
+    "if",
+    "else",
+    "while",
+    "return")
   private final val symbols = Array("{", "}", "(", ")", "[", "]",
     ".",
     ",",
@@ -43,19 +43,95 @@ case class Tokenizing(jackFile: File = null) {
     ">",
     "=",
     "~")
-  private val tokenArray = Array[String]()
+  // regular exprssions
+  val symbolReg = "[\\&\\*\\+\\(\\)\\.\\/\\,\\-\\]\\;\\~\\}\\|\\{\\>\\=\\[\\<]"
+  val intReg = "[0-9]+"
+  val strReg = "\"[^\"\n]*\""
+  val idReg = "[\\w_]+"
+  private var tokenArray = Array[String]()
 
   def parser(): Unit = {
     //remove comments
-    println("raed form :" + jackFile.getName)
+    println("read form :" + jackFile.getName)
     val code = removeComments(jackFile)
-    for (line <- code) println(line)
+    tokenArray = code.map(line => lineToTokens(line)).filterNot(_.equals("**ignore**")).toArray
+    println(tokenArray.toSeq)
+    writeXMlTokens()
 
   }
 
-  private def lineToTokens(line: String) {
+  private def lineToTokens(line: String): String = {
+    var isString = false
+    if (line.trim().length == 0 || line.equals("\n"))
+      return "**ignore**"
+    var myTokens = ""
+    var buffer = ""
+    for (ch <- line) {
+      breakable {
+        if (ch == '"') {
+          buffer += ch
+          isString = !isString
+          break //move to the next char
+        }
+        if (isString) {
+          buffer += ch
+          break // move to the next char
+        }
+
+        else if (symbols.contains(ch.toString)) {
+          //we reach to symbol in the code
+          if (!buffer.isEmpty && buffer.trim.length > 0)
+            myTokens += parseBuffertoken(buffer) + "\n"
+          myTokens += createSymbolToken(ch) + "\n"
+          buffer = ""
+        }
+        else if (ch == 32)
+        //we reach to white space in the code
+          {
+            if (!buffer.isEmpty && buffer.trim.length > 0)
+              myTokens += parseBuffertoken(buffer) + "\n"
+            buffer = ""
+          }
+          else
+          buffer = buffer + ch
+      }
+    }
+    return myTokens
+  }
+
+  def parseBuffertoken(buffer: String): String = {
+    var noSpacesBuffer = buffer.trim
+    if (buffer.matches("[0-9]+"))
+      return createConstIntegerToken(noSpacesBuffer)
+    else if (noSpacesBuffer(0) == '"')
+      return createStringConstantToken(noSpacesBuffer.slice(1, buffer.length - 1))
+    else
+      return createWordToken(noSpacesBuffer)
 
   }
+
+  def createWordToken(word: String): String = {
+    if (keyWords.contains(word))
+      return createXMlToken("keyword", word)
+    else return createXMlToken("identifier", word)
+  }
+
+  def createConstIntegerToken(number: String) = createXMlToken("integerConstant", number)
+
+  def createStringConstantToken(str: String) = createXMlToken("stringConstant", str)
+
+  def createSymbolToken(symbol: Char): String = {
+    symbol match {
+      case '<' => return createXMlToken("symbol", "&lt;")
+      case '>' => return createXMlToken("symbol", "&gt;")
+      case '&' => return createXMlToken("symbol", "&amp;")
+      case '"' => return createXMlToken("symbol", "&quet;")
+      case _ => return createXMlToken("symbol", symbol.toString)
+    }
+  }
+
+  private def createXMlToken(tokPattren: String, tokName: String): String =
+    "<" + tokPattren + "> " + tokName + " </" + tokPattren + ">"
 
   private def removeComments(file: File): Array[String] = {
 
@@ -67,7 +143,7 @@ case class Tokenizing(jackFile: File = null) {
 
     def removeBlockComments(strIn: String): String = {
       var startIndex = strIn.indexOf("/**")
-
+      strIn.length
       if (startIndex == -1) return strIn
 
       var result = strIn
@@ -76,9 +152,9 @@ case class Tokenizing(jackFile: File = null) {
       if (startIndex == 0 && endIndex == -1) {
         return ""
       }
-      while ( {
+      while (
         startIndex != -1
-      }) {
+      ) {
 
         if (endIndex == -1) return strIn.substring(0, startIndex - 1)
         result = result.substring(0, startIndex) + result.substring(endIndex + 2)
@@ -92,20 +168,17 @@ case class Tokenizing(jackFile: File = null) {
 
     var finalCode = Source.fromFile(file).getLines().map(line => removeSinglecomment(line)).toArray
     finalCode = finalCode.filterNot(x => x.equals("")).toArray
-    finalCode = finalCode.map(line => removeBlockComments(line)).toArray
+    finalCode = removeBlockComments(finalCode.mkString("\n")).split("\n")
 
-
-    return finalCode.filterNot(str => str.equals("") || str.startsWith(" *") || str.endsWith("*/"))
+    return finalCode.filter(_.nonEmpty)
 
   }
-
-  private def createXMlToken(tokPattren: String, tokName: String): String =
-    "<" + tokPattren + ">" + tokName + "</" + tokPattren + ">"
 
   private def writeXMlTokens(): Unit = {
 
     val fileName = jackFile.getPath.replaceAll(".jack", "TMaG.xml")
-    val root = "<tokens>" + tokenArray.mkString("\n") + "\n" + "</tokens>"
+    val root = "<tokens>" + "\n" + tokenArray.mkString("") + "</tokens>\n"
+
     val writer = new PrintWriter(new FileOutputStream(fileName, true))
     writer.append(root)
     writer.close()
